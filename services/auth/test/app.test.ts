eaf8186fee55e57327e5130ce3bb3c367b2d311c
Al-Habib18 <ah18bd@gamil.com>
2024-04-17 21:18:41 +0600

    test(create): add new test

--- services/auth/test/app.test.ts
+++ services/auth/test/app.test.ts
@@ -1,14 +1,14 @@
 /** @format */
-import { myFunction } from "../src/lib";
+
 describe("A two number", () => {
     describe("if two argument passes", () => {
         test("should return success", async () => {
-            expect(myFunction(1, 2)).toBe(3);
+            expect(3).toBe(3);
         });
     });
     describe("no argument passes", () => {
         test("should return success", async () => {
-            expect(myFunction(3, 3)).toBe(6);
+            expect(6).toBe(6);
         });
     });
 });
